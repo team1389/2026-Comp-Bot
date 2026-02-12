4415f3fd2d8bcc11b6330bed6a6306282b0993c5
Michæl <224322441+MPCode-Dev@users.noreply.github.com>
2026-02-12 15:36:31 -0500

    Update src/main/java/frc/command/RunFlywheel.java
    Changed to camel case
    
    Co-authored-by: jcballock <jcballock@users.noreply.github.com>

--- src/main/java/frc/command/RunFlywheel.java
+++ src/main/java/frc/command/RunFlywheel.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RunFlywheel extends Command {
     public FlywheelSubsystem flywheelSubsystem;
 
-    public RunFlywheel(FlywheelSubsystem FlywheelSubsystem) {
+    public RunFlywheel(FlywheelSubsystem flywheelSubsystem) {
         this.FlywheelSubsystem = FlywheelSubsystem;
     }
 
