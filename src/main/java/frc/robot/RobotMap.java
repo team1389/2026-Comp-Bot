fd03a298804c2c00687688441ddb8d65bf4b85b1
Schadsy <184569539+Schadsy@users.noreply.github.com>
2026-02-21 11:05:20 -0500

    Hood subsystem (#14)
    * Add Git Workflow Guide to README
    
    Added a comprehensive Git workflow guide for branch management and pull requests.
    
    * Fixing Vendordeps
    
    * Made HoodSubsystem from the example code and made it geared to the KrakenX44 motor - HenryS
    
    * Henry - Declared a lot of variables for the SmartMoterControllerConfig
    
    * Spotless
    
    ---------
    
    Co-authored-by: jcballock <jcballock@users.noreply.github.com>
    Co-authored-by: Laptop 44 <245260490+sneharawatmd@users.noreply.github.com>
    Co-authored-by: Akpinar <alexmeline1@gmail.com>

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,4 +13,23 @@ public class RobotMap {
   // Max Speeds
   public static final double MaxTurretSpeed = 1;
   public static final int FlywheelCanID = 20;
+
+  // Henry's Hood
+  public static final int HoodCanID = 2;
+  public static final double HoodIntegralCorr =
+      0.00016541; // How aggressivly hood fixes position error
+  public static final int HoodMaxAmp = 40; // Max motor current allowed (avoid brown out)
+  public static final double HoodRampRatePID =
+      .25; // In seconds time to ramp from 0 to full output in PID mode
+  public static final double HoodRampRateMan =
+      .25; // In seconds time to ramp from 0 to full output in manual mode
+  public static final int HoodGearRatio = 4;
+  public static final double HoodStaticVolts =
+      0.27937; // Volts needed to overcome static friction and make hood move
+  public static final double HoodVelVolts = 0.089836; // Volts needed to maintain const velocity
+  public static final double HoodAccVolts = 0.014557; // Voltage needed per unit of acceleration
+  public static final boolean HoodMoterInvert =
+      false; // True if the hood moter is inverted relative to the angle rotation
+  public static final int HoodMaxVel = 5000; // Max allowed rotational velocity in hood
+  public static final int HoodMaxAcc = 2500; // Max allowed velocity change in hood
 }
---
+++ src/main/java/frc/subsystems/HoodSubsystem.java
@@ -0,0 +1,113 @@
+package frc.subsystems;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import java.util.function.Supplier;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
+
+public class HoodSubsystem extends SubsystemBase {
+  private final TalonFX hoodMotor = new TalonFX(RobotMap.HoodCanID);
+
+  private final SmartMotorControllerConfig hoodMotorConfig =
+      new SmartMotorControllerConfig(this)
+          .withClosedLoopController(
+              RobotMap.HoodIntegralCorr,
+              0,
+              0,
+              RPM.of(RobotMap.HoodMaxVel),
+              RotationsPerSecondPerSecond.of(RobotMap.HoodMaxAcc))
+          .withGearing(
+              new MechanismGearing(GearBox.fromReductionStages(4, 4))) // gear ratio after reduction
+          .withIdleMode(MotorMode.COAST)
+          .withTelemetry("HoodMotor", TelemetryVerbosity.HIGH)
+          .withStatorCurrentLimit(Amps.of(RobotMap.HoodMaxAmp))
+          .withMotorInverted(RobotMap.HoodMoterInvert)
+          .withClosedLoopRampRate(Seconds.of(RobotMap.HoodRampRatePID))
+          .withOpenLoopRampRate(Seconds.of(RobotMap.HoodRampRateMan))
+          .withFeedforward(
+              new SimpleMotorFeedforward(
+                  RobotMap.HoodStaticVolts, RobotMap.HoodVelVolts, RobotMap.HoodAccVolts))
+          .withSimFeedforward(
+              new SimpleMotorFeedforward(
+                  RobotMap.HoodStaticVolts, RobotMap.HoodVelVolts, RobotMap.HoodAccVolts))
+          .withControlMode(ControlMode.CLOSED_LOOP);
+
+  private final SmartMotorController hoodSMC =
+      new TalonFXWrapper(hoodMotor, DCMotor.getKrakenX44(1), hoodMotorConfig);
+
+  private final ArmConfig hoodConfig =
+      new ArmConfig(hoodSMC)
+          .withTelemetry("HoodMech", TelemetryVerbosity.HIGH)
+          .withSoftLimits(Degrees.of(5), Degrees.of(100))
+          .withHardLimit(
+              Degrees.of(0), Degrees.of(120)); // The Hood can be modeled as an arm since it has a
+  // gravitational force acted upon based on the angle its in
+
+  private final Arm hood = new Arm(hoodConfig);
+
+  public HoodSubsystem() {}
+
+  public Command setAngle(Angle angle) {
+    return hood.setAngle(angle);
+  }
+
+  public void setAngleDirect(Angle angle) {
+    hoodSMC.setPosition(angle);
+  }
+
+  public Command setAngle(Supplier<Angle> angleSupplier) {
+    return hood.setAngle(angleSupplier);
+  }
+
+  public Angle getAngle() {
+    return hood.getAngle();
+  }
+
+  public Command sysId() {
+    return hood.sysId(
+        Volts.of(4.0), // maximumVoltage
+        Volts.per(Second).of(0.5), // step
+        Seconds.of(8.0) // duration
+        );
+  }
+
+  public Command setDutyCycle(Supplier<Double> dutyCycleSupplier) {
+    return hood.set(dutyCycleSupplier);
+  }
+
+  public Command setDutyCycle(double dutyCycle) {
+    return hood.set(dutyCycle);
+  }
+
+  @Override
+  public void periodic() {
+    hood.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    hood.simIterate();
+  }
+}
